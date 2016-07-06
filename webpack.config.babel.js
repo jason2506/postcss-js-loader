9b937e50ca8526d772c3ba1f2f658f0aeab0c1c8
Chi-En Wu <jason2506.mail@gmail.com>
2016-07-07 00:08:03 +0800

    Combine `css-in-js-loader` with `postcss-loader`

---
+++ webpack.config.babel.js
@@ -0,0 +1,47 @@
+import path from 'path'
+
+const library = 'postcss-js-loader'
+const srcPath = path.resolve(__dirname, 'src')
+const libPath = path.resolve(__dirname, 'lib')
+
+export default {
+  target: 'node',
+
+  devtool: 'source-map',
+
+  context: srcPath,
+
+  entry: 'index.js',
+
+  resolve: {
+    root: srcPath,
+  },
+
+  output: {
+    library,
+    libraryTarget: 'commonjs2',
+    filename: `${ library }.js`,
+    path: libPath,
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: srcPath,
+        loader: 'babel',
+      },
+    ],
+  },
+
+  externals: {
+    'loader-utils': 'loaderUtils',
+    postcss: 'postcss',
+    'postcss-js': 'postcssJs',
+    'webpack/lib/node/NodeTemplatePlugin': 'NodeTemplatePlugin',
+    'webpack/lib/node/NodeTargetPlugin': 'NodeTargetPlugin',
+    'webpack/lib/LibraryTemplatePlugin': 'LibraryTemplatePlugin',
+    'webpack/lib/SingleEntryPlugin': 'SingleEntryPlugin',
+    'webpack/lib/optimize/LimitChunkCountPlugin': 'LimitChunkCountPlugin',
+  },
+}
